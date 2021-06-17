0216322447717631cee1eeb247fd69de9ea420a4
Shrestha1202 <shreshthabansal12@gmail.com>
2021-06-17 17:41:12 +0530

    Third Commit

--- hello1/src/hello/JavaVersion.java
+++
@@ -1,8 +0,0 @@
-package hello;
-
-public class JavaVersion {
- public static void main(String agrs[])
- {
-	 System.out.println(System.getProperty("java.runtime.version"));
- }
-}
---
+++ src/hello/JavaVersion.java
@@ -0,0 +1,8 @@
+package hello;
+
+public class JavaVersion {
+ public static void main(String agrs[])
+ {
+	 System.out.println(System.getProperty("java.runtime.version"));
+ }
+}
